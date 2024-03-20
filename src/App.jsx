0b63668c3c666353a66989cab9fe46212abf8a9e
Gloria Lau <glorialau@live.ca>
2024-03-20 08:49:47 -0700

    add typography, chemistry cat image

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import HomePage from "./pages/HomePage/HomePage";
+import SelectionsPage from "./pages/SelectionsPage/SelectionsPage";
 import { Link } from "react-router-dom";
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<SelectionsPage />} />
           <Route />
         </Routes>
         <Footer />
